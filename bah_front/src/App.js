625384275c1756c1cdffd0409cff73bbd2428d05
Aaron Marino <aaron_marino@yahoo.com>
2020-09-02 11:27:09 -0400

    created react app, added local host, npm start and is working

---
+++ bah_front/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const baseUrl = 
+export default class App extends Component {
+  constructor(props) 
+  render() {
+    return (
+      <div>
+        <h1>It's working</h1>
+      </div>
+    )
+  }
+}
